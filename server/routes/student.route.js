607b60f894dadd3095625129540c35fee3784bea
chandanjainhp <chandanjaincj93@gmail.com>
2026-05-01 13:15:13 +0530

    Add authentication to student export endpoints (security fix)

--- server/routes/student.route.js
+++ server/routes/student.route.js
@@ -1,17 +1,10 @@
 import express from "express";
 
 import { selectStudent, exportCsv, exportPdf } from "../controllers/student.controller.js";
+import { verifyToken } from "../middleware/verifyToken.js";
+import { getClassRepository, finalizeSemester } from "../controllers/repository.controller.js";
 
 const router = express.Router();
-
-
-router.get("/students", selectStudent);
-router.get("/export/csv", exportCsv);
-router.get("/export/pdf", exportPdf);
-
-// Repository Routes
-import { getClassRepository, finalizeSemester } from "../controllers/repository.controller.js";
-import { verifyToken } from "../middleware/verifyToken.js";
 
 router.get("/repository", verifyToken, getClassRepository);
 router.post("/finalize", verifyToken, finalizeSemester);
